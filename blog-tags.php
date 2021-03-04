dfb843c33ff2abf122f384c5a404359ec900bf4e
Jon Gunderson <jongund@illinois.edu>
2021-03-04 16:38:53 -0600

    updated plugin

--- blog-tags.php
+++ blog-tags.php
@@ -9,38 +9,12 @@ Author URI: https://github.com/illinois-dres-aitg/blog-tags
 License: GPLv2
  */
 
-if (!function_exists('debug')) {
-    function debug($var = false, $showHtml = false, $showFrom = true) {
-        if ($showFrom) {
-            $calledFrom = debug_backtrace();
-            echo '<strong>';
-            echo substr(str_replace(ROOT, '', $calledFrom[0]['file']), 1);
-            echo '</strong>';
-            echo ' (line <strong>' . $calledFrom[0]['line'] . '</strong>)';
-        }
-        echo "\n<pre class=\"cake-debug\">\n";
+class BlogTags {
+    public function activate() {
 
-        $var = print_r($var, true);
-        if ($showHtml) {
-            $var = str_replace('<', '&lt;', str_replace('>', '&gt;', $var));
-        }
-        echo $var . "\n</pre>\n";
     }
 }
 
+$blog_tags = new BlogTags();
 
-define('WPCE_DIR_NAME', str_replace(basename(__FILE__), '', plugin_basename(__FILE__)));
-define('WPCE_URL', WP_PLUGIN_URL. '/' . WPCE_DIR_NAME);
-define('WPCE_DIR', WP_PLUGIN_DIR. '/' . WPCE_DIR_NAME);
-
-
-
-class Plugin {
-    public function activate() {}
-}
-
-
-
-$plugin = new Plugin();
-
-register_activation_hook(__FILE__, array($plugin, 'activate'));
+register_activation_hook(__FILE__, array($blog_tags, 'activate'));
