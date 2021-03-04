33e60bd88b403e4907b0ff90f26c329209a957d8
Jon Gunderson <jongund@illinois.edu>
2021-03-04 16:46:24 -0600

    updated plugin

--- blog-tags.php
+++ blog-tags.php
@@ -11,10 +11,21 @@ License: GPLv2
 
 class BlogTags {
     public function activate() {
+        global $wp_version;
+
+        if ( version_compare( $wp_version, '4.1', '<') ) {
+            wp_die('This plugin requires WordPress version 4.1 or higher');
+        }
+    }
+
+    public function deactivate() {
 
     }
+
 }
 
 $blog_tags = new BlogTags();
 
 register_activation_hook(__FILE__, array($blog_tags, 'activate'));
+
+register_deactivation_hook(__FILE__, array($blog_tags, 'deactivate'));
