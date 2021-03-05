b5c8b339f6ea61cab00692540d8166601e31744f
Jon Gunderson <jongund@illinois.edu>
2021-03-05 11:50:49 -0600

    updated code

--- blog-tags.php
+++ blog-tags.php
@@ -19,27 +19,27 @@ class BlogTags {
     }
 
     private function list_of_tags($tags) {
-        $html = '  <ul class="post_tags">\n';
+        $html = '  <ul class="post_tags">';
         foreach ( $tags as $tag ) {
             if ($tag->count > 0) {
                 $tag_link = get_tag_link( $tag->term_id );
 
-                $html .= "    <li><a href='{$tag_link}' class='{$tag->slug}'>";
-                $html .= "{$tag->name} ({$tag->count} posts)</a></li>\n";
+                $html .= "  <li><a href='{$tag_link}' class='{$tag->slug}'>";
+                $html .= "{$tag->name} ({$tag->count} posts)</a></li>";
             }
         }
-        $html .= '  </ul>\n';
+        $html .= '  </ul>';
         return $html;
     }
 
     public function show_tags() {
-        $html = '<div class="post_tags">\n';
+        $html = '<div class="post_tags">';
 
         $tags = get_tags(array(
               'taxonomy' => 'post_tag',
               'orderby' => 'count'));
 
-        $html .= '  <h2 class="post_tags">Tags by Popularity</h2>\n';
+        $html .= '  <h2 class="post_tags">Tags by Popularity</h2>';
 
         $html .= $this->list_of_tags($tags);
 
@@ -47,11 +47,11 @@ class BlogTags {
               'taxonomy' => 'post_tag',
               'orderby' => 'name'));
 
-        $html .= '  <h2 class="post_tags">Tags by Name</h2>\n';
+        $html .= '  <h2 class="post_tags">Tags by Name</h2>';
 
         $html .= $this->list_of_tags($tags);
 
-        $html .= '</div>\n';
+        $html .= '</div>';
         echo $html;
     }
 
