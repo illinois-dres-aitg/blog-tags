31ee77ad95a369c440d71ee20e5ccb1ceb183ad4
Jon Gunderson <jongund@illinois.edu>
2021-03-05 09:03:35 -0600

    updated

--- blog-tags.php
+++ blog-tags.php
@@ -20,14 +20,14 @@ class BlogTags {
 
     public function show_tags() {
         $tags = get_tags();
-        $html = '<div class="post_tags">';
+        $html = '<ul class="post_tags">';
         foreach ( $tags as $tag ) {
             $tag_link = get_tag_link( $tag->term_id );
 
-            $html .= "<a href='{$tag_link}' title='{$tag->name} Tag' class='{$tag->slug}'>";
-            $html .= "{$tag->name}</a>";
+            $html .= "<li><a href='{$tag_link}' title='{$tag->name} Tag' class='{$tag->slug}'>";
+            $html .= "{$tag->name}</a></li>";
         }
-        $html .= '</div>';
+        $html .= '</ul>';
         echo $html;
     }
 
